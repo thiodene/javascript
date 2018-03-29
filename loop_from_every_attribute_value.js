e4d993234c1c5a37c177a4041b9a6b5328572f23
Serge Ayissi <ayissi.serge@gmail.com>
2018-03-29 14:42:04 -0400

    Create loop_from_every_attribute_value.js

---
+++ loop_from_every_attribute_value.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+  $(document).ready(function()
+  {
+    // find all the Time input fields and apply the time select to them.
+    $("input[azbd_type='time']").ptTimeSelect();
+  });
+</script>
