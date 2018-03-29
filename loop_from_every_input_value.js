43d0bc31de052dcb68663e615461a83bfd999838
Serge Ayissi <ayissi.serge@gmail.com>
2018-03-29 14:44:07 -0400

    Create loop_from_every_input_value.js

---
+++ loop_from_every_input_value.js
@@ -0,0 +1,23 @@
+
+# Finds all inputs with a name attribute that contains 'man' and sets the value with some text.
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>attributeContains demo</title>
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+</head>
+<body>
+ 
+<input name="man-news">
+<input name="milkman">
+<input name="letterman2">
+<input name="newmilk">
+ 
+<script>
+$( "input[name*='man']" ).val( "has man in it!" );
+</script>
+ 
+</body>
+</html>
